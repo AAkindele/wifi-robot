93bb60d09bf3687adbbcbe7dce117fc4c350685a
AK Akindele <a.akindele3000@gmail.com>
2015-04-11 14:30:49 -0500

    following motor tutorial on adafruit. still cant get motor to move yet

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+// there are 513 steps per revolution
 
+#include <Stepper.h>
+
+int pin1 = 12;
+int pin2 = 11;
+int pin3 = 10;
+int pin4 = 9;
+
+Stepper motor(513, pin1, pin2, pin3, pin4);
+
+void setup() {
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+
+  Serial.begin(9600);
+  motor.setSpeed(20);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(Serial.available() > 0) {
+    int steps = Serial.parseInt();
+    Serial.print(steps);
+    motor.step(steps);
+  }
 }
