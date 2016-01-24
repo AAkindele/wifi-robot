760d78fdc853d2a5c91968bc675c58e91151ec56
AK Akindele <a.akindele3000@gmail.com>
2016-01-23 21:20:58 -0600

    wifi sheild is hooked up and working

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -1,28 +1,51 @@
-// there are 513 steps per revolution
+#include <Adafruit_CC3000.h>
+#include <ccspi.h>
+#include <SPI.h>
+#include <string.h>
+#include "utility/debug.h"
 
-#include <Stepper.h>
+#define ADAFRUIT_CC3000_IRQ  3
+#define ADAFRUIT_CC3000_VBAT 5
+#define ADAFRUIT_CC3000_CS   10
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
 
-int pin1 = 12;
-int pin2 = 11;
-int pin3 = 10;
-int pin4 = 9;
+#define WLAN_SSID "ssid"
 
-Stepper motor(513, pin1, pin2, pin3, pin4);
+#define WLAN_SECURITY WLAN_SEC_WPA2
 
-void setup() {
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
-  pinMode(pin3, OUTPUT);
-  pinMode(pin4, OUTPUT);
+#define WEBSITE "www.adafruit.com"
+#define WEBPAGE "/testwifi/index.html"
 
-  Serial.begin(9600);
-  motor.setSpeed(20);
+void setup(void)
+{
+  Serial.begin(115200);
+
+  Serial.println("\nInitializing...");
+  cc3000.begin();
+
+  Serial.println("enter password");
+  String pass;
+  while(Serial.available() == 0) {}
+  while(Serial.available() > 0) {
+    pass = Serial.readString();
+  }
+  char passChar[pass.length()];
+  pass.toCharArray(passChar, (pass.length() + 1));
+
+  Serial.print("\nAttempting to connect to "); Serial.println(WLAN_SSID);
+  cc3000.connectToAP(WLAN_SSID, passChar, WLAN_SECURITY);
+
+  Serial.println("Connected!");
+/*
+  // Wait for DHCP to complete 
+  Serial.println("Request DHCP");
+  cc3000.checkDHCP();
+*/
+  // You need to make sure to clean up after yourself or the CC3000 can freak out
+  // the next time your try to connect ...
+  Serial.println("\n\nDisconnecting");
+  cc3000.disconnect();
 }
 
-void loop() {
-  if(Serial.available() > 0) {
-    int steps = Serial.parseInt();
-    Serial.print(steps);
-    motor.step(steps);
-  }
+void loop(void){
 }
